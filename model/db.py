bcda6af3670f88d2518ae3ca0438a2a1f3cb15db
Mahdi Hosseinzade Bahreini <mhb8898@users.noreply.github.com>
2017-03-15 01:06:30 +0330

    tables & game logic

---
+++ model/db.py
@@ -0,0 +1,47 @@
+from flask_sqlalchemy import SQLAlchemy
+import jsonpickle
+import sqlalchemy.types as types
+from flask import Flask
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+
+class JsonType(types.MutableType, types.TypeDecorator):
+    impl = types.Unicode
+
+    def process_bind_param(self, value, engine):
+        return unicode(jsonpickle.encode(value))
+
+    def process_result_value(self, value, engine):
+        if value:
+            return jsonpickle.decode(value)
+        else:
+            # default can also be a list
+            return {}
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(80), unique=True)
+    student_num = db.Column(db.Integer, unique=True)
+
+    def __init__(self, name, num):
+        self.student_name = name
+        self.student_num = num
+
+    def __repr__(self):
+        return '<User %r>' % self.student_name
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    map= db.Column(JsonType())
+
+class Move(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), primary_key=True)
+    board_id=db.Column(db.Integer, db.ForeignKey(Board.id), primary_key=True)
+    user=db.relationship('User', foreign_keys='Move.user_id')
+    board=db.relationship('Board', foreign_keys='Move.board_id')
+    x=db.Column(db.Integer)
+    y=db.Column(db.Integer)
---
+++ model/map.py
@@ -0,0 +1,68 @@
+
+from enum import Enum
+from random import choice
+
+
+N=10
+
+class color(Enum):
+    red=1
+    blue=2
+    yellow=3
+    black=4
+    orange=5
+    # pig=7
+    # giant_pig=8
+    # rock=9
+    # wood=10
+    # glass=11
+def check(t,i,j,map):
+    if 0<=i<N and 0<=j<N and map[i][j] is t:
+        print i,j,t,map[i][j]
+        return True
+    else:
+        return False
+
+def dfs(i,j,map,seen=None):
+    if seen is None:
+        seen=set()
+    seen.add((i,j))
+    [dfs(i+x,j+y,map,seen) for x,y in [(1,0),(0,1),(-1,0),(0,-1)] if (i+x,j+y) not in seen and check(map[i][j],i+x,j+y,map)]
+    return seen
+
+def p(map):
+    for i in map.map:
+        print i
+
+class Map:
+    def __init__(self,map=None):
+        if map is None:
+            map = [[None]*N for _ in range(N)]
+        self.map=map
+        self.rand()
+    def rand(self):
+        self.gravity()
+        for i in range(N):
+            for j in range(N):
+                if self.map[i][j] is None:
+                    self.map[i][j]=choice(list(color))
+    def touch(self,i,j):
+        group_bird=dfs(i,j,self.map)
+        if len(group_bird)>1:
+            for i,j in group_bird:
+                self.map[i][j]=None
+            return True
+        else:
+            return False
+
+    def shift(self,i,j,c):
+        for f in range(i,N-c):
+            self.map[f][j],self.map[f+c][j]=self.map[f+c][j],self.map[f][j]
+
+    def gravity(self):
+        for j in range(N):
+            for i in range(N):
+                start=0
+                while i+start<N and self.map[i+start][j] is None:
+                    start+=1
+                self.shift(i,j,start)
